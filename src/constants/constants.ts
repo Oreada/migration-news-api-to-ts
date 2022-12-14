e98fb5dabcbf3faf4b0aceba2fbf576bf01a4edc
Olga Antushevich <antushevich@gmail.com>
2022-12-14 19:18:55 +0200

    deploy

--- src/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_KEY = 'ff7f57927f094fe1a429b319edc1e371';
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import App from './components/app/app';
-import './global.css';
-import Alphabet from './components/view/sources/alphabet';
-
-const app = new App();
-app.start();
-
-const example = new Alphabet();
-example.filtrateSources();
